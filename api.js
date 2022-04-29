66a716d2b3d14e5f60fe981eb3bddb109aed3803
Bozdogan <boraozdogan99@gmail.com>
2022-04-30 01:41:07 +0300

    increase test coverage

--- api.js
+++ api.js
@@ -1,25 +1,49 @@
 
 module.exports.isDiceCodeValid = function(dice_code) {
-    return  /^\d*d[1-9]\d*$/i.test(dice_code);
+    return  /^\d*d[1-9]\d*([+\-]\d+)?$/i.test(dice_code);
 }
 
-module.exports.roll = function(dice_code) {
-    const valid = isDiceCodeValid(dice_code);
-    console.log((valid ? "" : "IN") + "VALID", dice_code);
 
-    if(valid) {
-        const parts = dice_code.split("d");
-        const numRolls = parseInt(parts[0]);
-        const sides = parseInt(parts[1]);
-        console.log(numRolls, sides);
+module.exports._splitDiceLiteral = function(dice_code) {
+    const hasPlus = dice_code.includes("+");
+    const hasMinus = dice_code.includes("-");
 
-        let result = 0;
-        for(let i = 0; i < numRolls; ++i) {
-            result += 1 + Math.floor(Math.random()*sides);
-        }
-
-        return {dice_code, result}
+    let dicepart, constant;
+    if(hasPlus) {
+        const parts = dice_code.split("+");
+        dicepart = parts[0];
+        constant = parseInt(parts[1]);
+    } else if(hasMinus) {
+        const parts = dice_code.split("-");
+        dicepart = parts[0];
+        constant = -parseInt(parts[1]);
     } else {
-        return { error: "Invalid dice code"}
+        dicepart = dice_code;
+        constant = 0;
     }
+
+    const parts = dicepart.split("d");
+    const numRolls = parseInt(parts[0]) || 1;  // NOTE(bora): It may be omitted if there's only 1 die.
+    const sides = parseInt(parts[1]);
+    return [numRolls, sides, constant];
+}
+
+
+module.exports.roll = function(dice_code) {
+    const valid = this.isDiceCodeValid(dice_code);
+    if(!valid) {
+        return { dice_code, error: "Invalid dice code" };
+    }
+
+    const [ numRolls, sides, constant ] = this._splitDiceLiteral(dice_code);
+    console.log(numRolls, sides);
+
+    let result = 0;
+    for(let i = 0; i < numRolls; ++i) {
+        result += 1 + Math.floor(Math.random()*sides);
+    }
+
+    result += constant;
+
+    return { dice_code, result };
 }
--- test/apiTest.js
+++ test/apiTest.js
@@ -1,32 +1,114 @@
 const assert = require("chai").assert;
 const api = require("../api");
 
-const validCodes = ["2d10", "1d6", "4d3", "4d3-8", "1d6+4"]
-const invalidCodes = []
 
 describe("api", () => {
   describe("#isDiceCodeValid", () => {
+    it(`should return true for "2d10"`, () => {
+      assert.isTrue(api.isDiceCodeValid("2d10"));
+    });
+    
+    it(`should return true for "1d6"`, () => {
+      assert.isTrue(api.isDiceCodeValid("1d6"));
+    });
+    
+    it(`should return true for "4d3"`, () => {
+      assert.isTrue(api.isDiceCodeValid("4d3"));
+    });
+    
+    it(`should return true for "4d3-8"`, () => {
+      assert.isTrue(api.isDiceCodeValid("4d3-8"));
+    });
+    
+    it(`should return true for "1d6+4"`, () => {
+      assert.isTrue(api.isDiceCodeValid("1d6+4"));
+    });
+    
+    it(`should return true for "d6"`, () => {
+      assert.isTrue(api.isDiceCodeValid("d6"));
+    });
 
-    for(const code of validCodes) {
-      it(`should return true with value '${code}'`, () => {
-        assert.isTrue(api.isDiceCodeValid(code));
-      });
-    }
 
-    // it("should return true with value '2d10'", () => {
-    //   assert.isTrue(api.isDiceCodeValid("2d10"));
-    // });
-    // it("should return true with value '1d6'", () => {
-    //   assert.isTrue(api.isDiceCodeValid("1d6"));
-    // });
-    // it("should return true with value '4d3'", () => {
-    //   assert.isTrue(api.isDiceCodeValid("4d3"));
-    // });
-    // it("should return true with value '4d3-8'", () => {
-    //   assert.isTrue(api.isDiceCodeValid("4d3-8"));
-    // });
-    // it("should return true with value '1d6+4'", () => {
-    //   assert.isTrue(api.isDiceCodeValid("1d6+4"));
-    // });
+    it(`should return false for ""`, () => {
+      assert.isFalse(api.isDiceCodeValid(""));     
+    });
+    
+    it(`should return false for "hello"`, () => {   
+      assert.isFalse(api.isDiceCodeValid("hello"));        
+    });
+    
+    it(`should return false for "q=2d10"`, () => {  
+      assert.isFalse(api.isDiceCodeValid("q=2d10"));       
+    });
+    
+    it(`should return false for '"1d6"'`, () => {   
+      assert.isFalse(api.isDiceCodeValid('"1d6""'));        
+    });
+    
+    it(`should return false for "69d4:20"`, () => { 
+      assert.isFalse(api.isDiceCodeValid("69d4:20"));      
+    });
+    
+    it(`should return false for "34d500  "`, () => {
+      assert.isFalse(api.isDiceCodeValid("34d500  "));     
+    });
+  });
+
+  
+  describe("#_splitDiceLiteral", () => {
+    it(`should return [2, 10, 0] for "2d10"`, () => {
+      assert.deepEqual(api._splitDiceLiteral("2d10"), [2, 10, 0]);
+    });
+    
+    it(`should return [1, 4, 6] for "1d4+6"`, () => {
+      assert.deepEqual(api._splitDiceLiteral("1d4+6"), [1, 4, 6]);
+    });
+    
+    it(`should return [4, 3, -8] for "4d3-8"`, () => {
+      assert.deepEqual(api._splitDiceLiteral("4d3-8"), [4, 3, -8]);
+    });
+    
+    it(`should return [1, 6, 0] for "d6"`, () => {
+      assert.deepEqual(api._splitDiceLiteral("d6"), [1, 6, 0]);
+    });
+  });
+
+  
+  const ROLL_TEST_CONSTANT = 20;
+  
+  describe("#_roll", () => {
+    it(`should be in range [2, 20] for rolling "2d10"`, () => {
+      // NOTE(bora): Cannot seed Math.random(), so I'll basically
+      // run it a couple times to see if it breaks..
+      for(let i = 0; i < ROLL_TEST_CONSTANT; ++i) {
+        const { result } = api.roll("2d10");
+        assert.isAtLeast(result, 2);
+        assert.isAtMost(result, 20);
+      }
+    });
+
+    it(`should be in range [1, 6] for rolling "1d6"`, () => {
+      for(let i = 0; i < ROLL_TEST_CONSTANT; ++i) {
+        const { result } = api.roll("1d6");
+        assert.isAtLeast(result, 1);
+        assert.isAtMost(result, 6);
+      }
+    });
+
+    it(`should be in range [-1, 1] for rolling "d3-2"`, () => {
+      for(let i = 0; i < ROLL_TEST_CONSTANT; ++i) {
+        const { dice_code, result } = api.roll("d3-2");
+        assert.isAtLeast(result, -1);
+        assert.isAtMost(result, 1);
+      }
+    });
+
+    it(`should be in range [-4, 4] for rolling "4d3-8"`, () => {
+      for(let i = 0; i < ROLL_TEST_CONSTANT; ++i) {
+        const { result } = api.roll("4d3-8");
+        assert.isAtLeast(result, -4);
+        assert.isAtMost(result, 4);
+      }
+    });
   });
 });
